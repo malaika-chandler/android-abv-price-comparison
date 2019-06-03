3b718ee702e0087ce7245ff094378fb8572eb5fb
Malaika Chandler <malaika.r.chandler@gmail.com>
2019-06-03 09:19:24 -0700

    Added getter for entries and insert for new entry

--- app/src/main/java/com/example/abvpricecalculator/BeerEntryViewModel.java
+++ app/src/main/java/com/example/abvpricecalculator/BeerEntryViewModel.java
@@ -1,6 +1,29 @@
 package com.example.abvpricecalculator;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
 
 public class BeerEntryViewModel extends AndroidViewModel {
+
+    private BeerEntryRepository beerEntryRepository;
+    private LiveData<List<BeerEntry>> allEntries;
+
+    public BeerEntryViewModel(@NonNull Application application) {
+        super(application);
+        beerEntryRepository = new BeerEntryRepository(application);
+        allEntries = beerEntryRepository.getAllEntries();
+    }
+
+    public LiveData<List<BeerEntry>> getAllEntries() {
+        return allEntries;
+    }
+
+    public void insert(BeerEntry entry) {
+        beerEntryRepository.insert(entry);
+    }
 }
