8fa816a436087740eebc1d759629de3963b91b41
Malaika Chandler <malaika.r.chandler@gmail.com>
2019-06-03 08:41:51 -0700

    Filled in Dao details

--- app/src/main/java/com/example/abvpricecalculator/BeerEntryDao.java
+++ app/src/main/java/com/example/abvpricecalculator/BeerEntryDao.java
@@ -1,6 +1,21 @@
 package com.example.abvpricecalculator;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
+import java.util.List;
+
+@Dao
 public interface BeerEntryDao extends Dao {
+
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    void insert(BeerEntry entry);
+
+    @Query("DELETE FROM price_by_abv_list")
+    void deleteAll();
+
+    @Query("SELECT * from price_by_abv_list ORDER BY (price / ((abv / 100) * volume)) ASC")
+    List<BeerEntry> getAllEntries();
 }
