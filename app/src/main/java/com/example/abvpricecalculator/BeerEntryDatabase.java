950cd5b740653a8f76c737177ac839caa2d281c8
Malaika Chandler <malaika.r.chandler@gmail.com>
2019-06-03 08:57:49 -0700

    Added code to create initial db instance

--- app/src/main/java/com/example/abvpricecalculator/BeerEntryDatabase.java
+++ app/src/main/java/com/example/abvpricecalculator/BeerEntryDatabase.java
@@ -1,6 +1,28 @@
 package com.example.abvpricecalculator;
 
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {BeerEntry.class}, version = 1)
 public abstract class BeerEntryDatabase extends RoomDatabase {
+
+    public abstract BeerEntryDao beerEntryDao();
+
+    private static volatile BeerEntryDatabase INSTANCE;
+
+    static BeerEntryDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (BeerEntryDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            BeerEntryDatabase.class, "beer_entry_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
