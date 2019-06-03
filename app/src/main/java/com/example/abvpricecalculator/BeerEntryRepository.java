bed3b179d7ce5082048d54f653741e6ab52b0667
Malaika Chandler <malaika.r.chandler@gmail.com>
2019-06-03 09:11:35 -0700

    Added functionality to insert and retieve all entries; insert uses extended asyncTask class

--- app/src/main/java/com/example/abvpricecalculator/BeerEntryRepository.java
+++ app/src/main/java/com/example/abvpricecalculator/BeerEntryRepository.java
@@ -1,4 +1,42 @@
 package com.example.abvpricecalculator;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class BeerEntryRepository {
+
+    private BeerEntryDao beerEntryDao;
+    private LiveData<List<BeerEntry>> allEntries;
+
+    public BeerEntryRepository(Application application) {
+        BeerEntryDatabase db = BeerEntryDatabase.getDatabase(application);
+        beerEntryDao = db.beerEntryDao();
+        allEntries = beerEntryDao.getAllEntries();
+    }
+
+    public LiveData<List<BeerEntry>> getAllEntries() {
+        return allEntries;
+    }
+
+    public void insert(BeerEntry beerEntry) {
+        new insertAsyncTask(beerEntryDao).execute(beerEntry);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<BeerEntry, Void, Void> {
+        private BeerEntryDao asyncBeerEntryDao;
+
+        insertAsyncTask(BeerEntryDao beerEntryDao) {
+            this.asyncBeerEntryDao = beerEntryDao;
+        }
+
+        @Override
+        protected Void doInBackground(final BeerEntry... beerEntries) {
+            asyncBeerEntryDao.insert(beerEntries[0]);
+            return null;
+        }
+    }
 }
