a166e9783e64200015c516155500be321e8fdbf0
Malaika Chandler <malaika.r.chandler@gmail.com>
2019-06-07 13:14:37 -0700

    Style changes

--- app/src/main/java/com/example/abvpricecalculator/MainActivity.java
+++ app/src/main/java/com/example/abvpricecalculator/MainActivity.java
@@ -93,9 +93,7 @@ public class MainActivity extends AppCompatActivity implements AdapterDatabaseCa
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        } else if (id == R.id.delete_all) {
+        if (id == R.id.delete_all) {
             beerEntryViewModel.deleteAllEntries();
         }
 
